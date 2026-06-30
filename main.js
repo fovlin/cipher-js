f7e23d8979520f7853228087af48129349ed1095
fovlin <fovlin@163.com>
2026-06-30 11:28:48 +0800

    new

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as cipher from "./cipher.js";
 
-cipher.encryptFile("aes-128-ccm","tagore.txt",128,16)
+// cipher.encryptFile("aes-128-gcm","tagore.txt",128,16)
 
 // cipher.decryptFile('tagore.txt.enc','tagore.txt.key')
